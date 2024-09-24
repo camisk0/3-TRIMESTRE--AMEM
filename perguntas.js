f20826d69a19e3ad90f7997ad4046551e37d171e
camisk0 <165082371+camisk0@users.noreply.github.com>
2024-09-24 16:39:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar.'
+);
+console.log('Cartão de fotossíntese criado.');
+
+criaCartao(
+    'Biologia',
+    'Qual é a função dos ribossomos?',
+    'Ribossomos são responsáveis pela síntese de proteínas dentro da célula.'
+);
+console.log('Cartão de ribossomos criado.');
+
+criaCartao(
+    'Biologia',
+    'O que é a teoria da evolução?',
+    'A teoria da evolução, proposta por Charles Darwin, sugere que as espécies evoluem ao longo do tempo através da seleção natural.'
+);
+console.log('Cartão de teoria da evolução criado.');
+
+criaCartao(
+    'Biologia',
+    'O que é DNA?',
+    'DNA (Ácido Desoxirribonucleico) é a molécula que carrega a informação genética em todos os organismos vivos.'
+);
+console.log('Cartão de DNA criado.');
